2aab7149238b121878becbb189d62e1927913fc2
Dan McKay <mckayefc@gmail.com>
2014-03-19 00:30:34 +0000

    updated auth0 callback url

--- client/default/js/app.js
+++ client/default/js/app.js
@@ -28,7 +28,7 @@ var myApp = angular.module('myApp', [
     authProvider.init({
       domain:         'fh-dan.auth0.com',
       clientID:       'pEs0LRL0jgbs8fZpaiTHSS3qu44aknjo',
-      callbackURL:    '/',
+      callbackURL:    'https://psdev-ngjx1eo30qr3wz6wq5qti6lz-dev_psdev.df.dev.e111.feedhenry.net/',
       callbackOnLocationHash: true
     });
   }]);
